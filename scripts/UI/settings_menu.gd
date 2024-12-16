f33491573fc7926821cd07de9b5c8614c5eb6e38
Yuri-ga1 <regota09@gmail.com>
2024-12-16 22:37:40 +0800

    add sound settings

--- scripts/UI/settings_menu.gd
+++ scripts/UI/settings_menu.gd
@@ -2,19 +2,11 @@ extends Control
 
 signal back_pressed
 
-var master_volume = null
-var music_volume = null
-var sfx_volume = null
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	master_volume = $Audio/VBoxContainer/Master.value
-	music_volume = $Audio/VBoxContainer/Music.value
-	sfx_volume = $Audio/VBoxContainer/SFX.value
-
-	master_volume = db_to_linear(AudioServer.get_bus_volume_db(0))
-	music_volume = db_to_linear(AudioServer.get_bus_volume_db(1))
-	sfx_volume = db_to_linear(AudioServer.get_bus_volume_db(2))
+	$Audio/VBoxContainer/Master.value = db_to_linear(AudioServer.get_bus_volume_db(0))
+	$Audio/VBoxContainer/Music.value = db_to_linear(AudioServer.get_bus_volume_db(1))
+	$Audio/VBoxContainer/SFX.value = db_to_linear(AudioServer.get_bus_volume_db(2))
 
 
 func _on_back_pressed() -> void:
@@ -22,6 +14,6 @@ func _on_back_pressed() -> void:
 
 
 func _on_apply_pressed() -> void:
-	AudioServer.set_bus_volume_db(0, linear_to_db(master_volume))
-	AudioServer.set_bus_volume_db(1, linear_to_db(music_volume))
-	AudioServer.set_bus_volume_db(2, linear_to_db(sfx_volume))
+	AudioServer.set_bus_volume_db(0, linear_to_db($Audio/VBoxContainer/Master.value))
+	AudioServer.set_bus_volume_db(1, linear_to_db($Audio/VBoxContainer/Music.value))
+	AudioServer.set_bus_volume_db(2, linear_to_db($Audio/VBoxContainer/SFX.value))
