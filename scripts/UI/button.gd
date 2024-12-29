10f9e9f2478b628c056ab4f93a98a42c87f64bcd
Yuri-ga1 <regota09@gmail.com>
2024-12-29 21:44:22 +0800

    fix button alignment

--- scripts/UI/button.gd
+++ scripts/UI/button.gd
@@ -2,14 +2,22 @@ extends Button
 
 @export var button_text: String = "Default Text"
 
+var label: Label
+var texture_rect: TextureRect
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var label = $Texture/Text
-	var texture_rect = $Texture
+	label = $Texture/Text
+	texture_rect = $Texture
 	
 	label.text = button_text
+	var x_size = texture_rect.size.x
 	
 	var text_size = label.get_minimum_size()
 	texture_rect.size = text_size + Vector2(22, 22)
 	
+	if x_size != texture_rect.size.x:
+		var offset = texture_rect.size.x - x_size
+		texture_rect.position.x -= offset
+		
 	custom_minimum_size = texture_rect.size
